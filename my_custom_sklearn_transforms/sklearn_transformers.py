ac4ef27405851e74eafcd3043bea995db359befe
JMello123 <54131216+JMello123@users.noreply.github.com>
2020-08-17 02:24:58 -0300

    Update sklearn_transformers.py
    update_pre-processing

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -12,5 +13,16 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        # Retirando possivies alunos que não estão fazendo as provas
+        data = data[(data['NOTA_DE'] > 0) |
+          (data['NOTA_EM'] > 0) |
+          (data['NOTA_MF'] > 0) |
+          (data['NOTA_GO'] > 0) |
+          (data['H_AULA_PRES'] > 0)]
+        # Substituindo os valores nulos para NOTA_GO pela média
+        data['NOTA_GO'] = data['NOTA_GO'].fillna(round(data['NOTA_GO'].mean(),1))     
+        
+        # Preenchendo 0 os alunos que não apresentam dados sobre o ingles
+        data = data.fillna(0)
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
